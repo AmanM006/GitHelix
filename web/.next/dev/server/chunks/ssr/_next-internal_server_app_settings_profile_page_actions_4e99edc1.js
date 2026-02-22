f2e94ba5ff765439442bba427b08a54801b4da04
Aman <cheersaman1@gmail.com>
2026-02-22 21:15:09 +0530

    chore: remove build artifacts and clean repo structure

--- web/.next/dev/server/chunks/ssr/_next-internal_server_app_settings_profile_page_actions_4e99edc1.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/settings/profile/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_settings_profile_page_actions_4e99edc1.js.map
--- web/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"U7si9QVq3kKFFH4+WwzOKmAUozPW4etdUatgpk+LyDI=\"\n}"
